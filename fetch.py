932f51315086dacc94aec5969dcf450bc01873a3
30miin <spour.seemeen@gmail.com>
2020-12-02 23:49:29 +0330

    followed  by last commit !!

--- fetch.py
+++ fetch.py
@@ -1,1 +1,16 @@
-print("1234567890--098765456789")
+import pandas as pd
+import csv
+import glob
+
+
+
+
+df = pd.read_csv('ir-news-2-4.csv', encoding="utf-8")
+
+dfw = pd.DataFrame(df)
+text=[]
+
+
+text = dfw.content
+
+print(text)
