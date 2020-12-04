095e4afa2d33894d332eb428997a7a3cc4038082
30miin <spour.seemeen@gmail.com>
2020-12-05 00:33:29 +0330

    add stopwords

--- fetch.py
+++ fetch.py
@@ -4,26 +4,33 @@ import csv
 import glob
 import re
 text=[]
-
+tokens = []
+stopwords=[]
 
 def changes():
     df = pd.read_csv('ir-news-2-4.csv', encoding="utf-8") 
     text = df.content
-    print(text)
+    #print(text)
     for line in text:
         
         ac1 = re.sub(r"\d", " ", line)
         ac2 = re.sub(r'<[^>]+>', " ", ac1)
         ac3 = re.sub(r'[^\w\s]', ' ', ac2)
         ac4 = ac3
-        for l in ac4:
-            ac4 = re.sub(l, ' ', ac4)
-            tokens = ac4.split()
-            #print(tokens)
         # print(ac4)
-        # channel_values = open(ac4).read().split()
-        # print(channel_values)
+    
+        tokens = ac4.split()
+            
+        #print(tokens)
+        # with open("stopwords.txt") as fp: 
+        #     for line_S in fp:
+        #         print(line_S) 
 
+        text_file = open("stopwords.txt", "r")
+        lines = text_file.readlines()
+        print (lines)
+        
+        text_file.close()
         
 
 
