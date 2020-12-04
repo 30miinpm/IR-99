8e8e484026881e2f46998d2fe773669094d8ac2a
30miin <spour.seemeen@gmail.com>
2020-12-04 23:11:00 +0330

    ac4 ro mikham print bgirm , chikaresh konm ?thumbnail mizanw

--- fetch.py
+++ fetch.py
@@ -2,18 +2,22 @@ import numpy as np
 import pandas as pd
 import csv
 import glob
+import re
+text=[]
+ac4 = []
+
+def changes():
+    df = pd.read_csv('ir-news-2-4.csv', encoding="utf-8") 
+    text = df
+    #print(text)
+    for line in text:
+        ac1 = re.sub(r"\d", " ", line)
+        ac2 = re.sub(r'<[^>]+>', " ", ac1)
+        ac3 = re.sub(r'[^\w\s]', ' ', ac2)
+        ac4 = ac3
 
 
 
 
-df = pd.read_csv('ir-news-2-4.csv', encoding="utf-8")
-
-print(df)
-
-# dfw = pd.DataFrame(df)
-# text=[]
-
-
-# text = dfw.content
-
+changes()
 # print(text)
