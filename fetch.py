569752695f3a86e8643d89dd80fde80f07f11425
30miin <spour.seemeen@gmail.com>
2020-12-05 01:09:37 +0330

    stopwords not removed!

--- fetch.py
+++ fetch.py
@@ -8,35 +8,36 @@ tokens = []
 stopwords=[]
 
 def changes():
-    df = pd.read_csv('ir-news-2-4.csv', encoding="utf-8") 
+    df = pd.read_csv('ir-news-2-4.csv', encoding="utf-8")
     text = df.content
     #print(text)
     for line in text:
-        
+
         ac1 = re.sub(r"\d", " ", line)
         ac2 = re.sub(r'<[^>]+>', " ", ac1)
         ac3 = re.sub(r'[^\w\s]', ' ', ac2)
         ac4 = ac3
         # print(ac4)
-    
+
         tokens = ac4.split()
-            
+
         #print(tokens)
-        # with open("stopwords.txt") as fp: 
+        # with open("stopwords.txt") as fp:
         #     for line_S in fp:
-        #         print(line_S) 
+        #         print(line_S)
 
         lines =[]
         text_file = open("stopwords.txt", "r")
         lines = text_file.readlines()
 
-        
+
         text_file.close()
 
         new_list =[]
         for word in tokens:
             if word not in lines:
                 new_list.append(word)
+                tokens = new_list
     #print(new_list)
     x = len(lines)
     y = len(new_list)
